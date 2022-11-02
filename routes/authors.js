11cf6187620365f174a0349d33282ee8c6663c95
Makishi Asemota <makish.asemota@gmail.com>
2022-11-02 19:44:15 -0400

    added author edit, view, and delete routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Author = require("../models/author");
+const Book = require("../models/book");
 const router = express.Router();
 
 //All Authors Route
@@ -29,13 +30,73 @@ router.post("/", async (req, res) => {
 
   try {
     const newAuthor = await author.save();
-    //  res.redirect(`authors/${newAuthor.id}`)
-    res.redirect(`authors`);
+    res.redirect(`authors/${newAuthor.id}`);
   } catch {
     res.render("authors/new", {
       author: author,
       errorMessage: "Error creating Author",
     });
+  }
+});
+
+// Show Author Page
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/show", {
+      author: author,
+      booksByAuthor: books,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+// Edit Author Page
+router.get("/:id/edit", async (req, res) => {
+  try {
+    // mongoose method to find author in database
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", { author: author });
+  } catch {
+    res.redirect("/authors");
+  }
+});
+
+// Update Author Page
+router.put("/:id", async (req, res) => {
+  // define author outside of try/catch to use globally
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    }
+    res.render("authors/edit", {
+      author: author,
+      errorMessage: "Error updating Author",
+    });
+  }
+});
+
+// Delete author
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect("/authors");
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    }
+    res.redirect(`/authors/${author.id}`);
   }
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+// library for post forms
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -14,6 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: "false" }));
 
